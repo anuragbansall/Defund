dcbf9caa5cd58d44f4ffff7e537bdf75476fcf04
Anurag Bansal <devanuragbansal@gmail.com>
2026-01-25 13:49:38 +0530

    feat: remove Pexels API integration from package.json and findPhoto utility

--- client/src/utils/findPhoto.js
+++ client/src/utils/findPhoto.js
@@ -1,16 +1,18 @@
-import { createClient } from "pexels";
+// import { createClient } from "pexels";
 
-const client = createClient(import.meta.env.VITE_PEXELS_API_KEY);
+// const client = createClient(import.meta.env.VITE_PEXELS_API_KEY);
 
 const findPhoto = async (query) => {
-  try {
-    const photos = await client.photos.search({ query, per_page: 1 });
+  // try {
+  // const photos = await client.photos.search({ query, per_page: 1 });
 
-    return photos.photos[0].src.original;
-  } catch (error) {
-    console.error("Error fetching photo from Pexels:", error);
-    throw error;
-  }
+  // return photos.photos[0].src.original;
+
+  return "https://t4.ftcdn.net/jpg/04/39/89/01/360_F_439890152_sYbPxa1ANTSKcZuUsKzRAf9O7bJ1Tx5B.jpg";
+  // } catch (error) {
+  // console.error("Error fetching photo from Pexels:", error);
+  // throw error;
+  // }
 };
 
 export default findPhoto;
